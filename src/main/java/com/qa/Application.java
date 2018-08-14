d0280f69674a0694a21d711a9a24ef3b076e4870
nahrens12 <nathanahrens12@outlook.com>
2018-08-14 17:37:52 +0100

    Movie API

--- src/main/java/com/qa/Application.java
+++ src/main/java/com/qa/Application.java
@@ -1,9 +1,19 @@
 package com.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
+	
+	@Bean 
+	RestTemplate restTemplate()
+	{
+		return new RestTemplate(); 
+	}
+	
+	
     public static void main(String args[]) {
         SpringApplication.run(Application.class);
     }
