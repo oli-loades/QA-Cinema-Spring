2fd0ec3966d1b5d151ab46833069e2bf4d63f421
oli <oli.loades@gmail.com>
2018-08-16 12:47:58 +0100

    added del + update to order controller

--- src/main/java/com/qa/business/service/OrderService.java
+++ src/main/java/com/qa/business/service/OrderService.java
@@ -1,11 +1,13 @@
 package com.qa.business.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.qa.persistence.domain.OrderFilm;
+import com.qa.persistence.domain.Ticket;
 import com.qa.persistence.repository.OrderRepository;
 
 @Component
@@ -21,5 +23,19 @@ public class OrderService {
 	public String add(OrderFilm order) {
 		orderRepo.save(order);
 		return "{\"message\": \"order sucessfully added\"}";
+	}
+	
+	public String deleteOrder(long id) {
+		orderRepo.deleteById(id);
+		return "{\"message\": \"order sucessfully removed\"}";
+	}
+	
+	public String updateOrder(OrderFilm orderToUpdate,long id) {
+		Optional<OrderFilm> order = orderRepo.findById(id);
+		if (order.isPresent()) {
+			orderToUpdate.setId(id);
+		}
+		orderRepo.save(orderToUpdate);
+		return "{\"message\": \"ticket sucessfully updated\"}";
 	}
 }
--- src/main/java/com/qa/controller/OrderContoller.java
+++ src/main/java/com/qa/controller/OrderContoller.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import  org.springframework.web.bind.annotation.RestController;
 
 import com.qa.business.service.OrderService;
@@ -39,11 +42,18 @@ public class OrderContoller {
              return orderServ.add(orderFilmToAdd);
        }
 
-//       @PutMapping("{id}")
-//       @ResponseStatus(HttpStatus.CREATED)
-//       public void updateOrder(@PathVariable  )
-//       { orderReposoinity.
-//       }
+         @PutMapping("/update/{id}")
+       public String updateOrder(@RequestBody OrderFilm orderFilm, @PathVariable long id)
+       {
+            return orderServ.updateOrder(orderFilm, id);
+       }
+
+
+       @DeleteMapping("/delete/{id}")
+       public String detelteOrder(@PathVariable Long id)
+       {
+              return orderServ.deleteOrder(id);
+       }
 
 
 
