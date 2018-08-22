3fc493f20613f75a0dbe9a3a35996e07197bf5d9
kylemartindev <kyle2786@hotmail.com>
2018-08-22 13:40:42 +0100

    Seating fully working - third amendment adding data to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,9 @@ INSERT INTO orderfilm(id,account_id,movie) VALUES(1,1,1)
 INSERT INTO ticket(id,price,type) VALUES(1,1.5,'CHILD')
 
 INSERT INTO orderticket(id,order_id,ticket,quantity) VALUES(1,1,1,2)
+
+INSERT INTO screen (screennumber, movietitle) VALUES (1, 'Tag')
+
+INSERT INTO screen (screennumber, movietitle) VALUES (2, 'Open Wide')
+
+INSERT INTO screen (screennumber, movietitle) VALUES (3, 'Antman')
